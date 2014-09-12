466fe8be1de9bcc4dfcb5982ec7510055edbd389
kenokabe <kenokabe@gmail.com>
2014-09-12 19:24:14 +0900

    commit

---
+++ custom.zsh
@@ -0,0 +1,25 @@
+
+uname -r
+
+ZSH_THEME="ys"
+
+alias java="/cygdrive/c/Program\ Files/Java/jdk1.7.0_67/bin/java"
+
+alias mt5="/cygdrive/c/Users/ken/AppData/Roaming/MetaQuotes/Terminal/F792711EEED0C4A88615E52DD459D4CC/MQL5/Indicators"
+
+alias s="sh ~/bin/ss"
+alias sp="s; s push origin master"
+
+alias f="ranger"
+alias z="node app"
+alias x="node test"
+
+alias zz="browserify ./js/index.js -o ./js/index.bundle.js"
+alias a="atom ./"
+
+alias npm="cmd /c npm"
+
+
+alias kn="killall -9 node"
+
+alias t="tmux"
